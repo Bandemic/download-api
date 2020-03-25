f1caf72576e1f65f9a833c60ae7436062b28ac13
haveyaseen <haveyaread@gmail.com>
2020-03-25 15:33:55 +0100

    make since timestamp optional

--- db.py
+++ db.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_pymongo import PyMongo  # type: ignore
 from datetime import datetime
+from typing import Optional
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://172.31.16.148:27017/bluto"
 mongo = PyMongo(app)
 
 
-def get_cases(lat: float, lon: float, since: datetime):
+def get_cases(lat: float, lon: float, since: Optional[datetime]):
+    if since is None:
+        return mongo.db.cases.find({lat: lat, lon: lon})
     return mongo.db.cases.find({lat: lat, lon: lon, since: {"$gte": since}})
